b7cc2b75ec7372883e4cdae5388deb1fd5a5d967
SmilinJasper <smilin.jasper@yahoo.com>
2021-08-04 16:28:45 +0530

    refactor-emailjs-servicce-change-emailjs-sent-msg

--- assets/js/email_js_service.js
+++ assets/js/email_js_service.js
@@ -1,13 +1,11 @@
-window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
 
         // these IDs from the previous steps
         emailjs.sendForm('service_smilin_portfolio', 'template_2091tws', this)
-            .then(function() {
-                alert("Email Sent! I will get back to you soon")
-            }, function() {
+            .then(() => {
+                alert("Email Sent! I will get back to you soon.");
+            }, () => {
                 alert("Failed to send email, report issue to me please");
             });
     });
-}
